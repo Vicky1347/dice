969453c950ab63a9ee480ae43c27a79e8fbe27c7
Vicky1347 <110741340+Vicky1347@users.noreply.github.com>
2023-05-28 21:07:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var n=Math.random();
 n=n*6;
 n=Math.floor(n)+1;
-var player1="images/dice"+n+".png";
+var player1="dice"+n+".png";
 document.querySelector(".img1").setAttribute("src",player1);
 
 var m=Math.random()*6;
 
 m=Math.floor(m)+1;
-var player2="images/dice"+m+".png";
+var player2="dice"+m+".png";
 document.querySelector(".img2").setAttribute("src",player2);
 
 
