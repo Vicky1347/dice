761b196ba195d5de8c38db614e57c8f0cf0f3d9c
Vicky1347 <110741340+Vicky1347@users.noreply.github.com>
2023-05-28 21:05:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var n=Math.random();
+n=n*6;
+n=Math.floor(n)+1;
+var player1="images/dice"+n+".png";
+document.querySelector(".img1").setAttribute("src",player1);
+
+var m=Math.random()*6;
+
+m=Math.floor(m)+1;
+var player2="images/dice"+m+".png";
+document.querySelector(".img2").setAttribute("src",player2);
+
+
+if(n>m)
+{
+
+    document.querySelector("h1").innerHTML="Player 1 wins the match";
+}
+if(m>n)
+{
+    document.querySelector("h1").innerHTML="Player 2 wins the match";
+}
+else if(m==n)
+{
+
+    document.querySelector("h1").innerHTML="tie";
+}
