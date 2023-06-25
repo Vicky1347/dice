662396f392372066506c0752533d937968427dcd
Vicky1347 <110741340+Vicky1347@users.noreply.github.com>
2023-06-25 22:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ var m=Math.random()*6;
 m=Math.floor(m)+1;
 var player2="dice"+m+".png";
 document.querySelector(".img2").setAttribute("src",player2);
-
+var ply1=prompt("enter Player1 Nmae");
+var ply2=prompt("enter Player2 Nmae");
 
 if(n>m)
 {
 
-    document.querySelector("h1").innerHTML="Player 1 wins the match";
+    document.querySelector("h1").innerHTML= ply1+" wins the match";
 }
 if(m>n)
 {
-    document.querySelector("h1").innerHTML="Player 2 wins the match";
+    document.querySelector("h1").innerHTML=ply2 +" wins the match";
 }
 else if(m==n)
 {
